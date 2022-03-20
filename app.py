fa4d600695610c3ecad8cd5626c0e1ed414f06fa
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-03-20 18:55:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 
 import streamlit as st
-import pymongo
-import pandas as pd
-import pymongo
-from pymongo import MongoClient
-import streamlit.components.v1 as components
-import matplotlib.pyplot as plt
-import requests
-import streamlit.components.v1 as components
-import numpy as np
-from datetime import datetime, timezone
-import statsmodels 
-from statsmodels.tsa.seasonal import seasonal_decompose
-import plotly.tools
-from twilio.rest import Client #Twilio is a service that delivers SMS
-import scipy
-import seaborn as sns
+
 
 def main():
     page = st.sidebar.selectbox("Select a Page",["Project", "raw data","analysed data"])
